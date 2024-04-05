8037bd2123630ed8792dc7d2509224642aa99150
Georgia <georgiamayridge97@gmail.com>
2024-04-05 22:39:34 +1300

    uploadJSONFiles

--- FrontEnd/src/components/Journey-Nav.jsx
+++ FrontEnd/src/components/Journey-Nav.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import "./Journey-Nav.css";
 import JourneyNavData from "./Journey-Nav-Data";
 import Add from "../assets/Add.png";
@@ -10,26 +11,30 @@ function JourneyNav(){
             <h1>Your Journeys</h1>
             <p>You can navigate, export and view your trips below</p>
             <div className="journeycard">
-                <JourneyNavData
-                image={Add}
-                heading="Add New Journey"
-                text="Add a new journey to your list"
-                />
-
-<JourneyNavData
-                image={List}
-                heading="View All Journeys"
-                text="View, edit and add comments to your journeys"
-                />
-
-<JourneyNavData
-                image={Export}
-                heading="Export Journey List"
-                text="Export your journey list to a CSV, PDF or Excel file for your records"
-                />
-                </div>
+                <a href="/journey/form">
+                    <JourneyNavData
+                        image={Add}
+                        heading="Add New Journey"
+                        text="Add a new journey to your list"
+                    />
+                </a>
+                <a href="/journey/list">
+                    <JourneyNavData
+                        image={List}
+                        heading="View All Journeys"
+                        text="View, edit and add comments to your journeys"
+                    />
+                </a>
+                <a href="/journey/export">
+                    <JourneyNavData
+                        image={Export}
+                        heading="Export Journey List"
+                        text="Export your journey list to a CSV, PDF or Excel file for your records"
+                    />
+                </a>
+            </div>
         </div>
-    )
+    );
 }
 
 export default JourneyNav;
